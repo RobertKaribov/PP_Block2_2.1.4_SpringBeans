b72c5993e4fd10bed973068364bdb59ce2283cfe
RobertKaribov <infobox.markets@gmail.com>
2024-06-08 22:36:14 +0300

    2

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -7,22 +7,24 @@ import org.springframework.stereotype.Component;
 @Component
 public class AnimalsCage {
 
-    @Qualifier("dog")
     @Autowired
+    @Qualifier("dogBean")
     private Animal animal;
 
     @Autowired
     private Timer timer;
 
+    public Timer getTimer() {
+        return timer;
+    }
+
     public void whatAnimalSay() {
         System.out.println("Say:");
         System.out.println(animal.toString());
         System.out.println("At:");
-        System.out.println(new Timer().getTime());
+        System.out.println(timer.getTime());
         System.out.println("________________________");
     }
 
-    public Timer getTimer() {
-        return timer;
-    }
+
 }
--- src/main/java/app/model/Dog.java
+++ src/main/java/app/model/Dog.java
@@ -1,7 +1,9 @@
 package app.model;
 import org.springframework.stereotype.Component;
 
-@Component
+@Component("dogBean")
 public class Dog extends Animal {
-
+    public String toString() {
+        return "Im a Dog";
+    }
 }
